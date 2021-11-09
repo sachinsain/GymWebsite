9445a9a3a0f1b0e188c1f4dee3e775df64698584
sachinsain <sachinsain43436@gmail.com>
2021-11-09 14:39:38 +0530

    frist commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const Port = 3000;
+const fs= require('fs')
+const express=require('express')
+const app=express();
+const Home=fs.readFileSync('index.html')
+const about=fs.readFileSync('about.html')
+app.use('/static',express.static('static'));
+app.get('/',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.send(Home);
+})
+app.get('/about',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.send(about)
+})
+app.listen(Port,()=>{
+    console.log('server is running')
+})
